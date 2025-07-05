319394b5af765cee032f76f89a381844ddcb3915
Ahmad <crisitiano678@gmail.com>
2025-07-05 23:17:23 +0500

    Package Update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["aromatic-shark-272.convex.cloud"],
+  },
 };
 
 export default nextConfig;
